4e4983ff72f284253f738d6e8b165e2d7f826a04
Poorna Gottimukkula <Poorna.Gottimukkula1@ibm.com>
2024-04-01 15:25:15 +0530

    updated code
    Signed-off-by: Poorna Gottimukkula <Poorna.Gottimukkula1@ibm.com>

--- vars/cleanupOcp4Cluster.groovy
+++ vars/cleanupOcp4Cluster.groovy
@@ -17,9 +17,9 @@ def call()
         sh (returnStdout: false, script: "cd ${WORKSPACE}/deploy && make $TARGET:clean || true")
         sh (returnStdout: false, script: "cd ${WORKSPACE}/deploy && make $TARGET:clean || true")
         sh (returnStdout: false, script: "cd ${WORKSPACE}/deploy && make $TARGET:clean || true")
-        sh(returnStatus: false, returnStdout: false, script: "openstack  --os-auth-url \"${env.OS_AUTH_URL}\" --insecure flavor delete ${env.MASTER_TEMPLATE}")
-        sh(returnStatus: false, returnStdout: false, script: "openstack  --os-auth-url \"${env.OS_AUTH_URL}\" --insecure flavor delete ${env.WORKER_TEMPLATE}")
-        sh(returnStatus: false, returnStdout: false, script: "openstack  --os-auth-url \"${env.OS_AUTH_URL}\" --insecure flavor delete ${env.BOOTSTRAP_TEMPLATE}")
-        sh(returnStatus: false, returnStdout: false, script: "openstack  --os-auth-url \"${env.OS_AUTH_URL}\" --insecure flavor delete ${env.BASTION_TEMPLATE}")
+        sh(returnStatus: false, returnStdout: false, script: "openstack  --os-auth-url \"${env.OS_AUTH_URL}\" --insecure flavor delete ${env.MASTER_TEMPLATE} || true")
+        sh(returnStatus: false, returnStdout: false, script: "openstack  --os-auth-url \"${env.OS_AUTH_URL}\" --insecure flavor delete ${env.WORKER_TEMPLATE} || true")
+        sh(returnStatus: false, returnStdout: false, script: "openstack  --os-auth-url \"${env.OS_AUTH_URL}\" --insecure flavor delete ${env.BOOTSTRAP_TEMPLATE} || true")
+        sh(returnStatus: false, returnStdout: false, script: "openstack  --os-auth-url \"${env.OS_AUTH_URL}\" --insecure flavor delete ${env.BASTION_TEMPLATE} || true")
     }
 }
