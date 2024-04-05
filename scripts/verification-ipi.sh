86bdd962e98c1e910bd9fd12a4a7edad909de6dc
Poorna Gottimukkula <Poorna.Gottimukkula1@ibm.com>
2024-04-05 12:33:42 +0530

    Enable weekly e2e on ipi job
    Signed-off-by: Poorna Gottimukkula <Poorna.Gottimukkula1@ibm.com>

---
+++ scripts/verification-ipi.sh
@@ -0,0 +1,71 @@
+#!/bin/bash -x
+
+echo "Setting up htpasswd"
+htpasswd -c -B -b users.htpasswd testuser1 testuser1
+oc create secret generic htpass-secret --from-file=htpasswd=users.htpasswd -n openshift-config
+
+cat << EOF| oc apply -f -		
+apiVersion: config.openshift.io/v1
+kind: OAuth
+metadata:
+  name: cluster
+spec:
+  identityProviders:
+  - name: my_htpasswd_provider
+    mappingMethod: claim
+    type: HTPasswd
+    htpasswd:
+      fileData:
+        name: htpass-secret		
+EOF
+
+oc get secret htpass-secret -ojsonpath={.data.htpasswd} -n openshift-config | base64 --decode > users.htpasswd
+htpasswd -bB users.htpasswd testuser2 testuser2
+htpasswd -bB users.htpasswd testuser2 testuser3
+htpasswd -bB users.htpasswd testuser2 testuser4
+oc create secret generic htpass-secret --from-file=htpasswd=users.htpasswd --dry-run=client -o yaml -n openshift-config | oc replace -f -
+echo "sleeping for 3m" && sleep 3m
+
+oc login -u testuser1 -p testuser1
+oc login -u testuser2 -p testuser2
+oc login -u testuser3 -p testuser3
+oc login -u testuser4 -p testuser4
+
+echo "Setting up environmental variables"
+OC_URL=$(oc whoami --show-server)
+OC_URL=$(echo $OC_URL | cut -d':' -f2 | tr -d [/])
+OC_CONSOLE_URL=$(oc whoami --show-console)
+ver_cli=$(oc version --client | grep -i client | cut -d ' ' -f 3 | cut -d '.' -f1,2)
+export BUSHSLICER_DEFAULT_ENVIRONMENT=ocp4
+export OPENSHIFT_ENV_OCP4_HOSTS=$OC_URL:lb
+export OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS=testuser1:testuser1,testuser2:testuser2,testuser3:testuser3,testuser4:testuser4
+export OPENSHIFT_ENV_OCP4_ADMIN_CREDS_SPEC=file:///home/jenkins/.kube/config
+export BUSHSLICER_CONFIG="{'global': {'browser': 'chrome'}, 'environments': {'ocp4': {'admin_creds_spec': '/home/jenkins/.kube/config', 'api_port': '6443', 'web_console_url': '${OC_CONSOLE_URL}', 'version': '${ver_cli}.0'}}}"
+echo $BUSHSLICER_DEFAULT_ENVIRONMENT
+echo $OPENSHIFT_ENV_OCP4_HOSTS
+echo $OPENSHIFT_ENV_OCP4_USER_MANAGER_USERS
+echo $OPENSHIFT_ENV_OCP4_ADMIN_CREDS_SPEC
+echo $BUSHSLICER_CONFIG
+
+cd ../
+echo "Setting up environment for verification tests"
+# sudo yum module list ruby
+# sudo dnf module reset ruby -y
+# sudo yum install -y @ruby:3.1
+ruby --version
+
+echo "Cloning verification-tests repo"
+git clone https://github.com/openshift/verification-tests.git
+cd verification-tests
+sed -i "s/gem 'watir'/gem 'watir', '7.2.0'/g" Gemfile #for running inside the container
+sed -i "s/gem 'azure-storage'/#gem 'azure-storage'/g" Gemfile
+sed -i "s/gem 'azure_mgmt_storage'/#gem 'azure_mgmt_storage'/g" Gemfile
+sed -i "s/gem 'azure_mgmt_compute'/#gem 'azure_mgmt_compute'/g" Gemfile
+sed -i "s/gem 'azure_mgmt_resources'/#gem 'azure_mgmt_resources'/g" Gemfile
+sed -i "s/gem 'azure_mgmt_network'/#gem 'azure_mgmt_network'/g" Gemfile
+sed -i "s/BUSHSLICER_DEBUG_AFTER_FAIL=true/BUSHSLICER_DEBUG_AFTER_FAIL=false/g" config/cucumber.yml
+git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/openshift/cucushift.git features/tierN/
+#./tools/install_os_deps.sh
+#./tools/hack_bundle.rb
+bundle install
+bundle exec cucumber --no-color --tags '@ppc64le and @'${ver_cli}' and @network-ovnkubernetes and not @inactive and not @destructive and not @fips and not @upgrade and @heterogeneous'
